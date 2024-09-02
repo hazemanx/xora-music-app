bdaaf0931d25c042e46d568562feaba4afeff419
hazemanx <nhayes227@gmail.com>
2024-09-02 14:46:58 -0400

    Create home.js

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Welcome to XORA</h1>
+      <p className="text-xl">Discover and enjoy high-quality, ad-free music.</p>
+    </div>
+  );
+}
+
+export default Home;
