b1f26955682ca22b1b3e0a15d503e74fa071b6af
hazemanx <nhayes227@gmail.com>
2024-09-02 14:47:32 -0400

    Create search.js

---
+++ src/pages/search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Search</h1>
+      <p className="text-xl">Find your favorite artists, songs, and podcasts.</p>
+    </div>
+  );
+}
+
+export default Search;
